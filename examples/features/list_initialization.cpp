bca05bff74745b95829ee6d34af9dbd074c45e38
linhsu0723 <linhsu0723@qq.com>
2019-04-07 18:12:52 +0800

    features/list_initialization.cpp

---
+++ examples/features/list_initialization.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	{
+		int i{}; // 0
+		// int i{0}; // 0
+		// int i = {}; // 0
+		// int i = {0}; // 0
+		cout << i << endl;
+	}
+	cout << "----------" << endl;
+	{
+		int i{1}; // 1
+		// int i = {1}; // 1
+		cout << i << endl;
+	}
+	cout << "----------" << endl;
+	{
+		int a[2][2]; // random
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++)
+				cout << a[i][j] << " ";
+			cout << endl;
+		}
+	}
+	cout << "----------" << endl;
+	{
+		int a[2][2]{}; // all 0
+		// int a[2][2] = {}; // all 0
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++)
+				cout << a[i][j] << " ";
+			cout << endl;
+		}
+	}
+	cout << "----------" << endl;
+	{
+		int a[2][2]{1}; // 1 0; 0 0
+		// int a[2][2] = {1}; // 1 0; 0 0
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++)
+				cout << a[i][j] << " ";
+			cout << endl;
+		}
+	}
+}
