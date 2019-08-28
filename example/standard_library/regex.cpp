6cd5c71781fe62ea00bb5b521bc9736e4d6edef3
linhsu0723 <linhsu0723@qq.com>
2019-08-28 22:57:56 +0800

    standard_library/regex: more

--- example/standard_library/regex.cpp
+++ example/standard_library/regex.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int main() {
+    // regex_match()
     {
         // https://www.regular-expressions.info/floatingpoint.html
         regex re("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$"); // floating-point number
@@ -50,6 +51,8 @@ int main() {
 
     {
         wregex re(L"C\\+\\+是(世界|地球|宇宙)上最[好棒帅酷美]的语言！");
+        // No escape for back slashes in raw string literal.
+        wregex re_raw(LR"(C\+\+是(世界|地球|宇宙)上最[好棒帅酷美]的语言！)");
         wsmatch match;
 
         {
@@ -66,6 +69,39 @@ int main() {
             wstring s = L"PHP是世界上最好的语言！";
             assert(regex_match(s, match, re) == false);
         }
+    }
+
+
+
+    // regex_search()
+    {
+        string s = "Roses are #ff3300.";
+        regex color_re(
+            "#([a-f0-9]{2})"
+            "([a-f0-9]{2})"
+            "([a-f0-9]{2})");
+        assert(regex_search(s, color_re));
+
+        smatch color_match;
+        assert(regex_search(s, color_match, color_re));
+        assert(color_match.prefix() == "Roses are ");
+        assert(color_match.suffix() == ".");
+        assert(color_match.size() == 4);
+        assert(color_match[0] == "#ff3300");
+        assert(color_match[1] == "ff");
+        assert(color_match[2] == "33");
+        assert(color_match[3] == "00");
+    }
+
+
+
+    // regex_replace()
+    {
+        string s = "Quick brown fox.";
+        regex vowel_re("a|e|i|o|u");
+
+        assert(regex_replace(s, vowel_re, "*") == "Q**ck br*wn f*x.");
+        assert(regex_replace(s, vowel_re, "[$&]") == "Q[u][i]ck br[o]wn f[o]x.");
     }
 
     return 0;
