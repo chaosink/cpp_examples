9f01e5f32db6b3f2b310223baca971259e2cdf19
linhsu0723 <linhsu0723@qq.com>
2020-05-10 18:12:02 +0800

    metaprogramming/policy/policy_container: remove unused code

--- example/metaprogramming/policy/policy_container.hpp
+++ example/metaprogramming/policy/policy_container.hpp
@@ -11,13 +11,4 @@ constexpr bool IsPolicyContainer = false;
 template <typename...T>
 constexpr bool IsPolicyContainer<PolicyContainer<T...>> = true;
 
-template <typename TLayerName, typename...TPolicies>
-struct SubPolicyContainer;
-
-template <typename T>
-constexpr bool IsSubPolicyContainer = false;
-
-template <typename TLayer, typename...T>
-constexpr bool IsSubPolicyContainer<SubPolicyContainer<TLayer, T...>> = true;
-
 }
