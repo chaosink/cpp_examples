fec6c761d3cf4542e27df74a07c90520adf4bcc6
linhsu0723 <linhsu0723@qq.com>
2020-05-15 00:27:54 +0800

    metaprogramming/has_type_: function template

--- example/metaprogramming/has_type_.cpp
+++ example/metaprogramming/has_type_.cpp
@@ -12,12 +12,31 @@ using Void = void;
 
 HAS_TYPE(bar)
 
+
+
+#define HAS_TYPE_(NAME)                                \
+    template<typename T>                               \
+    std::true_type _has_type_##NAME(typename T::foo*); \
+    template<typename T>                               \
+    std::false_type _has_type_##NAME(...);             \
+    template<typename T>                               \
+    using has_type_##NAME = decltype(_has_type_##NAME<T>(nullptr));
+
+HAS_TYPE_(foo)
+
+
+
 struct A {
     using bar = int;
 };
-struct B {};
+struct B {
+    using foo = float;
+};
 
 int main() {
     static_assert(has_type_bar<A>::value == true);
     static_assert(has_type_bar<B>::value == false);
+
+    static_assert(has_type_foo<A>::value == false);
+    static_assert(has_type_foo<B>::value == true);
 }
